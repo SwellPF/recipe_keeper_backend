a903ba3b200223ac9dc971c8a12b4b4d576e406c
Paul Fell <swellpf@gmail.com>
2021-05-09 17:53:27 -0400

    Updating category and recipes controllers, ingred

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::RecipesController < ApplicationController
+
+    def index
+        recipes = Recipe.all
+        render json: recipes
+    end
+
+    def create
+        recipe.new(recipe_params)
+        if recipe.save
+            render json: recipe, status: :accepted
+        else
+            render json: {errors: recipe.errors.full_messages}, status: unprocessible_entity
+        end
+    end
+
+            private
+
+            def recipe_params
+                params.require(:recipe).permit(name, directions, :category_id)
+            end
+end
