67a90f6f8ac9de9a23fe78772a6978479d2c965d
Paul Fell <swellpf@gmail.com>
2021-05-11 15:55:19 -0400

    Added delete method to recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -20,6 +20,14 @@ class Api::V1::RecipesController < ApplicationController
     end
 
 
+    def destroy
+            recipe = Recipe.find(params[:id])
+            if recipe.destroy
+              render json: { id: recipe.id }
+            end
+    end
+
+
             private
 
             def recipe_params
